e1aa5c450fdf7f6f128b34b7a6805cd1ef577b6e
Fred-grais <frederic.grais@gmail.com>
2014-12-20 20:12:53 +0100

    Merge with Thomas + fixed Gmap problems

--- app/src/main/java/fr/utt/if26/istarve/views/RestaurantsCarteFragment.java
+++ app/src/main/java/fr/utt/if26/istarve/views/RestaurantsCarteFragment.java
@@ -6,12 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 import fr.utt.if26.istarve.R;
 
 /**
  * Created by Thomas on 17/12/2014.
  */
 public class RestaurantsCarteFragment extends android.support.v4.app.Fragment {
+    private static final String TAG = RestaurantsCarteFragment.class.getSimpleName();
+
     public RestaurantsCarteFragment() {
 
     }
@@ -22,6 +26,17 @@ public class RestaurantsCarteFragment extends android.support.v4.app.Fragment {
         // Inflate the layout for this fragment
         View RestaurantCarteView = inflater.inflate(R.layout.fragment_restaurants_carte, null);
         return RestaurantCarteView;
+    }
+
+    // onDestroyView() Needed to allow switching back to the map tab and avoid the duplicate id error
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.v(TAG, "HEreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+        SupportMapFragment f = (SupportMapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        if (f != null)
+            getFragmentManager().beginTransaction().remove(f).commit();
     }
 
 }
